7acd19ba1fc12879281b5f88f7c3043ac27812d1
Fairgraves <rozaco0@gmail.com>
2024-09-28 11:17:36 +0700

    <feature> Создать mapperы через mapstruct

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,44 +1,21 @@
 package ru.practicum.shareit.item.mapper;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import ru.practicum.shareit.item.dto.ItemCreateDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemUpdateDto;
 import ru.practicum.shareit.item.model.Item;
 
-@Component
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class ItemMapper {
-    public ItemDto mapToItemDto(Item item) {
-        ItemDto itemDto = new ItemDto();
-        itemDto.setName(item.getName());
-        itemDto.setAvailable(item.getAvailable());
-        itemDto.setDescription(item.getDescription());
-        itemDto.setId(item.getId());
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ItemMapper {
+    ItemDto mapToItemDto(Item item);
 
-        return itemDto;
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "owner", ignore = true)
+    Item mapCreateDtoToItem(ItemCreateDto itemDto);
 
-    public Item mapCreateDtoToItem(ItemCreateDto itemDto) {
-        Item item = new Item();
-        item.setName(itemDto.getName());
-        item.setAvailable(itemDto.getAvailable());
-        item.setDescription(itemDto.getDescription());
-
-        return item;
-
-    }
-
-    public Item mapUpdateDtoToItem(ItemUpdateDto itemUpdateDto) {
-        Item item = new Item();
-        item.setId(itemUpdateDto.getId());
-        item.setName(itemUpdateDto.getName());
-        item.setDescription(itemUpdateDto.getDescription());
-        item.setAvailable(itemUpdateDto.getAvailable());
-
-        return item;
-
-    }
+    @Mapping(target = "owner", ignore = true)
+    Item mapUpdateDtoToItem(ItemUpdateDto itemUpdateDto);
 }
