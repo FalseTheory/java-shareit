853bfdbd7e49df1564e5975ef089830a5e373a6c
Fairgraves <rozaco0@gmail.com>
2024-10-06 14:38:46 +0700

    <feature> create database schema

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.dto.UserCreateDto;
@@ -10,6 +11,7 @@ import ru.practicum.shareit.user.dto.UserUpdateDto;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
     UserDto mapToUserDto(User user);
+    @Mapping(target = "id", ignore = true)
     User mapCreateDtoToUser(UserCreateDto userDto);
     User mapUpdateDtoToUser(UserUpdateDto userUpdateDto);
 }
